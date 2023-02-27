36445806b7494c55f54a11a2d911deee1a31ef6f
Lautaro Asat <lautaronorielasat@gmail.com>
2023-02-26 21:09:45 -0300

    Add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mynerve: ["Mynerve", "cursive"],
+        indie: ["Amatic SC", "cursive"],
+        code: ["Source Code Pro", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
