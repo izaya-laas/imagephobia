dd4d612b7ec2dc8b19446268f9fcbdf4fbe37706
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-03 15:02:18 -0300

    Add loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center mt-4">
+      <img className="w-12" src="/public/loader.svg" alt="loader" />
+    </div>
+  );
+}
