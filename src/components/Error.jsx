57ca90520b3625553c555907584175d41a0e6799
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-03 15:02:38 -0300

    Add error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error({ error }) {
+  return (
+    <div className="mt-2 px-2 py-1 text-white rounded-lg font-code max-w-xl mx-auto bg-[#f9523c]">
+      {error}
+    </div>
+  );
+}
