a7ef368682aa541045636bc7359055738c07a235
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-03 15:04:40 -0300

    Add load, error and send states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,42 +1,104 @@
-import { signal } from "@preact/signals";
+import { useState } from "preact/hooks";
+import { validateURL } from "../../helpers/validateUrl";
+import Error from "./Error";
+import Loading from "./Loading";
 
-const valuePage = signal("");
+const validationURL = (url) => {
+  if (url === "") {
+    return "You need to add a page";
+  }
 
-export default function Form() {
-  const sendData = async (e) => {
+  if (!validateURL(url)) {
+    return "The url page is not corrrect";
+  }
+
+  return null;
+};
+
+export default function Form({ setResponse }) {
+  const [formData, setFormData] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(error);
+
+  const sendData = async (e, setResponse) => {
     e.preventDefault();
-    const url = valuePage.value;
+    setResponse(null);
 
-    if (url === "") return console.error("URL Vacia");
-
-    const response = await fetch(`http://localhost:8080/prueba`, {
+    const url = formData;
+    const options = {
       method: "POST",
       headers: {
         "Content-Length": 0,
         "Content-Type": "application/json",
       },
       body: url,
-    }).then((res) => res.json());
+    };
+    const responseValidation = validationURL(url);
+    setError(responseValidation);
 
-    console.log(response);
+    if (responseValidation) {
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+
+      return;
+    }
+
+    setIsLoading(true);
+
+    await fetch(`http://localhost:8080/prueba`, options)
+      .then((res) => {
+        console.log(res);
+
+        return res.ok ? res.json() : Promise.reject(res);
+      })
+      .then(async (json) => {
+        console.log("BIEN");
+        console.log(json);
+        setResponse(json);
+      })
+      .catch((err) => {
+        console.log("ERROR");
+        const { statusText, status } = err;
+
+        const error = { name: statusText, status };
+
+        setError(error);
+
+        setTimeout(() => {
+          setError(null);
+        }, 5000);
+      });
+
+    setIsLoading(false);
+    // setResponse(response);
+
+    // console.log(response);
   };
 
   return (
-    <form
-      className="flex font-code items-center gap-y-2 w-full max-w-xl mx-auto rounded-md text-white border"
-      onSubmit={sendData}
-    >
-      <input
-        className="block w-full py-4 rounded-l-sm text-2xl text-[#1d1d1d] text-center placeholder:text-[#dadce0] focus:outline-none"
-        placeholder="find website"
-        type="text"
-        id="for"
-        value={valuePage}
-        onChange={(e) => (valuePage.value = e.target.value)}
-      />
-      <button className="block px-4 py-4 border-l rounded-r-sm font-bold">
-        <img className="w-6" src="/public/find.svg" alt="find icon" />
-      </button>
-    </form>
+    <>
+      <form
+        className="flex font-code items-center gap-y-2 w-full max-w-xl mx-auto rounded-md text-white border"
+        onSubmit={(e) => sendData(e, setResponse)}
+      >
+        <input
+          className="block w-full py-4 rounded-l-sm text-2xl text-[#1d1d1d] text-center placeholder:text-[#dadce0] focus:outline-none"
+          placeholder="find website"
+          required
+          type="text"
+          id="for"
+          value={formData}
+          onChange={(e) => setFormData(e.target.value)}
+        />
+        <button className="block px-4 py-4 border-l rounded-r-sm font-bold">
+          <img className="w-6" src="/public/find.svg" alt="find icon" />
+        </button>
+      </form>
+      {error?.status && <Error error={error.name} />}
+      {isLoading && <Loading />}
+    </>
   );
 }
