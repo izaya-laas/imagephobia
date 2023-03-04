987ff31d5e5c985be5b717937049c63086df5ac6
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-04 14:35:08 -0300

    Add response data

---
+++ src/components/ImagesResponse.jsx
@@ -0,0 +1,44 @@
+import { byteToKilobyte } from "../../helpers/ByteToKilobyte.js";
+import { calculatePercentaje } from "../../helpers/calculatePercentaje.js";
+import ButtonDownloadAll from "./ButtonDownloadAll.jsx";
+import CardImage from "./CardImage.jsx";
+
+export default function ImagesResponse({ images }) {
+  const imagesOptimized = images.map((image) => image.optimizedImage);
+
+  return (
+    <section className="">
+      <h3 className="text-center font-code font-bold mt-4 mb-2">
+        We found <span className="text-[#1a73e8]">{images.length}</span>{" "}
+        non-optimized images
+      </h3>
+      <ButtonDownloadAll images={imagesOptimized} />
+      <article className="flex flex-col gap-y-4 justify-center max-w-3xl px-2 sm:px-6 mx-auto">
+        {images.map(({ optimizedImage, oldImage }) => {
+          const { url, bytes: bytesOptimized } = optimizedImage;
+          const { bytes: bytesOld } = oldImage;
+
+          const percentajeInFavor = calculatePercentaje(
+            bytesOld,
+            bytesOptimized
+          );
+
+          const kilobytesOptimized = byteToKilobyte(bytesOptimized);
+          const kilobytesOld = byteToKilobyte(bytesOld);
+
+          const urlDownload = url.replace("upload/", "upload/fl_attachment/");
+
+          return (
+            <CardImage
+              kilobytesOld={kilobytesOld}
+              kilobytesOptimized={kilobytesOptimized}
+              url={url}
+              urlDownload={urlDownload}
+              percentajeInFavor={percentajeInFavor}
+            />
+          );
+        })}
+      </article>
+    </section>
+  );
+}
