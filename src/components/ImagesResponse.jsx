b557f25eef4826c3bf6d7ba4d11baca6debbc80c
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-04 16:01:27 -0300

    Add condition to render data

--- src/components/ImagesResponse.jsx
+++ src/components/ImagesResponse.jsx
@@ -7,38 +7,45 @@ export default function ImagesResponse({ images }) {
   const imagesOptimized = images.map((image) => image.optimizedImage);
 
   return (
-    <section className="">
-      <h3 className="text-center font-code font-bold mt-4 mb-2">
+    <section>
+      <h3 className="text-center text-lg font-code font-bold mt-4 mb-2">
         We found <span className="text-[#1a73e8]">{images.length}</span>{" "}
-        non-optimized images
+        unoptimized images
       </h3>
-      <ButtonDownloadAll images={imagesOptimized} />
-      <article className="flex flex-col gap-y-4 justify-center max-w-3xl px-2 sm:px-6 mx-auto">
-        {images.map(({ optimizedImage, oldImage }) => {
-          const { url, bytes: bytesOptimized } = optimizedImage;
-          const { bytes: bytesOld } = oldImage;
+      {images.length > 0 && (
+        <>
+          <ButtonDownloadAll images={imagesOptimized} />
+          <article className="flex flex-col gap-y-4 justify-center max-w-3xl px-2 sm:px-6 mx-auto">
+            {images.map(({ optimizedImage, oldImage }) => {
+              const { url, bytes: bytesOptimized } = optimizedImage;
+              const { bytes: bytesOld } = oldImage;
 
-          const percentajeInFavor = calculatePercentaje(
-            bytesOld,
-            bytesOptimized
-          );
+              const percentajeInFavor = calculatePercentaje(
+                bytesOld,
+                bytesOptimized
+              );
 
-          const kilobytesOptimized = byteToKilobyte(bytesOptimized);
-          const kilobytesOld = byteToKilobyte(bytesOld);
+              const kilobytesOptimized = byteToKilobyte(bytesOptimized);
+              const kilobytesOld = byteToKilobyte(bytesOld);
 
-          const urlDownload = url.replace("upload/", "upload/fl_attachment/");
+              const urlDownload = url.replace(
+                "upload/",
+                "upload/fl_attachment/"
+              );
 
-          return (
-            <CardImage
-              kilobytesOld={kilobytesOld}
-              kilobytesOptimized={kilobytesOptimized}
-              url={url}
-              urlDownload={urlDownload}
-              percentajeInFavor={percentajeInFavor}
-            />
-          );
-        })}
-      </article>
+              return (
+                <CardImage
+                  kilobytesOld={kilobytesOld}
+                  kilobytesOptimized={kilobytesOptimized}
+                  url={url}
+                  urlDownload={urlDownload}
+                  percentajeInFavor={percentajeInFavor}
+                />
+              );
+            })}
+          </article>
+        </>
+      )}
     </section>
   );
 }
