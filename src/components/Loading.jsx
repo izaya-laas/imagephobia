03cad018ff146a48b655b1f27b7b18423b2fd5ab
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-04 16:01:43 -0300

    Change style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center mt-20">
       <img className="w-12" src="/public/loader.svg" alt="loader" />
     </div>
   );
