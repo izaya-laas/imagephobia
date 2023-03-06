9263e4607a603a9fd7563b6c28250bd986fdf7f3
Lautaro Asat <lautaronorielasat@gmail.com>
2023-03-06 10:05:42 -0300

    Delete signals

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,33 +1,35 @@
-import { signal } from "@preact/signals";
+import { useState } from "preact/hooks";
 
-const isActive = signal(false);
-export const configImages = signal({
-  ieCompatibility: localStorage.getItem("ieCompatibility") || false,
-});
-
-function handleIsActive() {
-  isActive.value = !isActive.value;
-}
-
-function changeIECompatibility() {
-  configImages.value.ieCompatibility = !configImages.value.ieCompatibility;
-  localStorage.setItem("ieCompatibility", configImages.value.ieCompatibility);
-}
+const initialIECompatibility = localStorage.getItem("ieCompatibility") || false;
 
 export default function Config() {
+  const [isActive, setIsActive] = useState(false);
+  const [ieCompatibility, setIECompatibility] = useState(
+    initialIECompatibility
+  );
+
+  function handleIsActive() {
+    setIsActive(!isActive);
+  }
+
+  function changeIECompatibility() {
+    setIECompatibility(!ieCompatibility);
+    localStorage.setItem("ieCompatibility", !ieCompatibility);
+  }
+
   return (
     <>
       <img
         onClick={handleIsActive}
-        className={`absolute top-4 right-2 sm:top-6 sm:right-6 w-12 ${
-          isActive.value ? "animate-spin" : ""
+        className={`top-4 right-2 sm:top-6 sm:right-6 w-12 ${
+          isActive ? "animate-spin fixed" : "absolute"
         }`}
         src="/public/gear.svg"
         alt="Config Icon"
       />
       <aside
         className={`fixed top-20 right-0 pl-2 pr-1 sm:px-2 py-2 backdrop-blur-md bg-[white]/40 border border-y-black border-l-black rounded-l-sm transition-transform duration-700 before:contents before:w-2 before:bg-black before:h-2 before:absolute before:top-0 before:left-0 ${
-          isActive.value ? "" : "translate-x-full"
+          isActive ? "" : "translate-x-full"
         }`}
       >
         <div className="w-full">
